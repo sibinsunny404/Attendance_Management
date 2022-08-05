f41e9eabf859e844f6005668abc7814e72fcc9e5
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-05 07:37:51 +0530

    09/05/22

--- Attendance/sample.php
+++ Attendance/sample.php
@@ -6,43 +6,33 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-</head>
-<body>
   <link rel="stylesheet" href="css/submitbtt.css">
-  <!DOCTYPE html>
-<html lang="en">
-<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Submit Button</title>
-    
+    <script>
+    // $(window).load(function() {
+    // $(".load-wraper").fadeOut("slow");
+    $(window).on("load", function () {
+  document.getElementByClassName("loade-wrapper").style.visibility = "hidden";
+});
+  // });
+</script>
 </head>
 <body>
+  <!-- <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Fside-view-male-hacker-with-gloves-laptop_23-2148578161.jpg%3Fw%3D2000&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fhacker&tbnid=Va4sajl16Uao9M&vet=12ahUKEwiE1beDxq75AhWe_TgGHTvRDTUQMygAegUIARCdAQ..i&docid=ZvN5iDZAawZDPM&w=2000&h=1333&q=hacker%20image&ved=2ahUKEwiE1beDxq75AhWe_TgGHTvRDTUQMygAegUIARCdAQ" alt=""> -->
   <center>
-  <link rel="stylesheet" href="css/loader.css">
-  <div class="load-wraper">
-<div class="spinner">
-  <div class="set1">  
-    <div class="ball"></div>
-    <div class="ball"></div>
-  </div>
-  <div class="set2">
-    <div class="ball"></div>
-    <div class="ball"></div>
-  </div>
-</div>
+    <img src="https://picsum.photos/300/300/?random" alt="">
+ <!-- Loading square for squar.red network -->
+<link rel="stylesheet" href="css/loader.css">
+<div class="loader-wraper">
+<span class="loader"><span class="loader-inner"></span></span>
 </div>
 <script>
-  $(window).on("load",function(){
-    $(".load-wraper").fadeOut("slow");
+  $(window).on(load,function(){
+    $(".loader-wraper").fadeOut("slow");
   });
 </script>
-  <div class="container">
-  <div class="button button-1">Neon</div>
-  </div>
     </center>
 </body>
-</html>
-  </body>
 </html>
