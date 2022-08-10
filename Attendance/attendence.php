1fc90069e8e01eacd60230872dd547a899829f5f
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-10 22:54:43 +0530

    10/08//22

--- Attendance/attendence.php
+++ Attendance/attendence.php
@@ -9,24 +9,13 @@ $result = mysqli_query($connect, $query);
 if(isset($_POST['submit'])){
     foreach($_POST['attend'] as $ids=>$attend){
        $usn= $_POST["usn"] [$ids];
-       $sub=$_POST["sub"] [$ids];
+    //    $sub=$_POST["sub"] [$ids];
+    mysqli_query($connect,"insert into attendence (usn,attend) values ('$usn','$attend')");
     }
     // echo $sub;
        
         // $sem=$_POST["sem"];
-        // mysqli_query($connect,"insert into attendence (usn,attend) values ('$usn','$attend')");
-        ?>
-       <div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
-  <div class="d-flex">
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-    <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-  </div>
-</div>
-<?php
-        // echo $check_id;
-       
+        
 }
 mysqli_close($connect);
 //Insert Query Ends here
