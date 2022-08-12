fb25be930d6f46b665a676f8cfb820df587cec09
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-12 23:22:33 +0530

    12/08/22final

--- Attendance/admin_dash.php
+++ Attendance/admin_dash.php
@@ -168,17 +168,11 @@
 					    </li><!--//nav-item-->
 					    <li class="nav-item">
 					        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-					        <a class="nav-link" href="#">
+					        <a class="nav-link" href="addstudent.php">
 						        <span class="nav-icon">
-						        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-  <path fill-rule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"/>
-  <circle cx="3.5" cy="5.5" r=".5"/>
-  <circle cx="3.5" cy="8" r=".5"/>
-  <circle cx="3.5" cy="10.5" r=".5"/>
-</svg>
+								<img style="height: 20px;width:20px;" src="assets/images/extra/add_stu.png" alt="">
 						         </span>
-		                         <span class="nav-link-text">Orders</span>
+		                         <span class="nav-link-text">Add Student</span>
 					        </a><!--//nav-link-->
 					    </li><!--//nav-item-->
 					    <li class="nav-item has-submenu">
@@ -194,7 +188,7 @@
 		                        
 
 					   
-		                         <span class="nav-link-text">Charts</span>
+		                         <span class="nav-link-text">View Students</span>
 					        </a><!--//nav-link-->
 					    </li><!--//nav-item-->
 					    
