f3e34c35ce665fda422bc8acd82d9dbf72b33e94
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-22 22:50:29 +0530

    22/08/22

--- Attendance/addstudent.php
+++ Attendance/addstudent.php
@@ -1,140 +1,146 @@
 <!-- Add student-->
 <?php
-  require_once 'sessions.php';
-  require_once 'database/connection.php';
-  if (isset($_POST['add'])) {
-        $name = $_POST["uname"];
-        $usn = $_POST["usn"];
-        $cls = $_POST["class"];
-        $sem = $_POST["sem"];
-        $dstr = $_POST["dst"];
-        $dob = $_POST["dob"];
-        $state = $_POST["state"];
-        $mbno = $_POST["mbno"];
-        $add = $_POST["add"];
-        mysqli_query($connect, "insert into students (usn,name,class,sem,dob,mbno,district,state,address) 
+require_once 'sessions.php';
+require_once 'database/connection.php';
+if (isset($_POST['add'])) {
+  $name = $_POST["uname"];
+  $usn = $_POST["usn"];
+  $cls = $_POST["class"];
+  $sem = $_POST["sem"];
+  $dstr = $_POST["dst"];
+  $dob = $_POST["dob"];
+  $state = $_POST["state"];
+  $mbno = $_POST["mbno"];
+  $add = $_POST["add"];
+  mysqli_query($connect, "insert into students (usn,name,class,sem,dob,mbno,district,state,address) 
         values ('$usn','$name','$cls','$sem','$dob','$mbno','$dstr','$state','$add')");
-        header("location:admin_dash.php?adds=student added success");
-    }
+  header("location:admin_dash.php");
+}
 //  $name=$_POST['uname'];
 //  echo $name;
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Add students</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <style>
-        body{
-            background: #355C7D;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+<head>
+  <title>Add students</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        }
-    </style>
-    <script>
-            function done(){
-                alert("Student Detials Successfully Added...");
-            }
-        </script>
-  </head>
-  <body>
-  <script>
-        window.addEventListener("load", () => {
-            const loader = document.querySelector(".loader");
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <style>
+    body {
+      background: #355C7D;
+      /* fallback for old browsers */
+      background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);
+      /* Chrome 10-25, Safari 5.1-6 */
+      background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);
+      /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+  </style>
+  <script src="jquery-3.6.0.min.js"></script>
 
-            loader.classList.add("loader--hidden");
-
-            loader.addEventListener("transitionend", () => {
-                document.body.removeChild(loader);
-            });
+  <!-- <script>
+  $(function(){
+  //   $("#name").keyup(function(){
+  $('#name, #usn, #mbno,#add').keyup(function(){
+      $("#submit").removeAttr('disabled');
+    });
         });
-    </script>
-    <link rel="stylesheet" href="css/loader.css">
-    <div class="loader" style="z-index:100;"></div>
-    <div style="border:0px;" class="header_fixed">
-<center>
-<form method="POST">
-<div class="container" style="padding-top: 125px;">
-    <div class="card" style="width: 50rem; border-color:crimson;
+  </script> -->
+</head>
+
+<body>
+  <script>
+    window.addEventListener("load", () => {
+      const loader = document.querySelector(".loader");
+
+      loader.classList.add("loader--hidden");
+
+      loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+      });
+    });
+  </script>
+  <link rel="stylesheet" href="css/loader.css">
+  <div class="loader" style="z-index:100;"></div>
+  <div style="border:0px;" class="header_fixed">
+    <center>
+      <form method="POST">
+        <div class="container" style="padding-top: 125px;">
+          <div class="card" style="width: 50rem; border-color:crimson;
    background: #BE93C5;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #7BC6CC, #BE93C5);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #7BC6CC, #BE93C5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 border: radius 10%;
 ">
 
-  <div class="card-body">
-    
-  <h2 style="font-style:italic;">Enter The Student Detials</h2>
-    <div class="row g-3">
- 
-  <div class="col-md-6">
-    <label for="name" class="form-label">Full Name</label>
-    <input type="text" class="form-control" id="name" name="uname" required>
-  </div>
-  <div class="col-md-6">
-    <label for="usn" class="form-label">USN</label>
-    <input type="text" class="form-control" id="usn" name="usn" required>
-  </div>
-  <div class="col-md-4">
-    <label for="class" class="form-label">CLASS</label>
-    <select id="class" class="form-select" name="class" required>
-      <option selected>Choose...</option>
-      <option value="mca">MCA</option>
-    </select>
-  </div>
-  <div class="col-md-4">
-    <label for="sem" class="form-label">SEM</label>
-    <select id="sem" class="form-select" name="sem" required>
-      <option selected>Choose...</option>
-      <option value="2">2</option>
-    </select>
-  </div>
-  <div class="col-md-4">
-    <label for="dob" class="form-label">DOB</label>
-    <input type="date" class="form-control" id="dob" name="dob" required>
-  </div>
-  <div class="col-md-4">
-    <label for="dst" class="form-label">District</label>
-    <select id="dst" class="form-select" name="dst" required>
-      <option selected>Choose...</option>
-      <option value="dk">DK</option>
-    </select>
-  </div>
-  <div class="col-md-4">
-    <label for="state" class="form-label">State</label>
-    <select id="state" class="form-select" name="state" required>
-      <option selected>Choose...</option>
-      <option value="karnataka">Karnataka</option>
-    </select>
-  </div>
-  <div class="col-md-4">
-    <label for="mbno" class="form-label">Mobile Number</label>
-    <input type="text" class="form-control" id="mbno" name="mbno" required>
-  </div>
-  <div class="col-12">
-    <label for="add" class="form-label">Address</label>
-    <input type="text" class="form-control" id="add" placeholder="Enter The full Address" name="add" required>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary" name="add" onclick="done()">ADD</button>
-  </div>
-      </div>
-    </div>
+            <div class="card-body">
 
-    </div>
-</div>
+              <h2 style="font-style:italic;">Enter The Student Detials</h2>
+              <div class="row g-3">
 
-</center>
-</form>
+                <div class="col-md-6">
+                  <label for="name" class="form-label">Full Name</label>
+                  <input type="text" class="form-control" id="name" name="uname" required>
+                </div>
+                <div class="col-md-6">
+                  <label for="usn" class="form-label">USN</label>
+                  <input type="text" class="form-control" id="usn" name="usn" required>
+                </div>
+                <div class="col-md-4">
+                  <label for="class" class="form-label">CLASS</label>
+                  <select id="class" class="form-select" name="class" required>
+                    <option value="mca">MCA</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <label for="sem" class="form-label">SEM</label>
+                  <select id="sem" class="form-select" name="sem" required>
+                    <option value="2">2</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <label for="dob" class="form-label">DOB</label>
+                  <input type="date" class="form-control" id="dob" name="dob" required>
+                </div>
+                <div class="col-md-4">
+                  <label for="dst" class="form-label">District</label>
+                  <select id="dst" class="form-select" name="dst" required>
+                    <option value="dk">DK</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <label for="state" class="form-label">State</label>
+                  <select id="state" class="form-select" name="state" required>
+                    <option value="karnataka">Karnataka</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <label for="mbno" class="form-label">Mobile Number</label>
+                  <input type="text" class="form-control" id="mbno" name="mbno" required>
+                </div>
+                <div class="col-12">
+                  <label for="add" class="form-label">Address</label>
+                  <input type="text" class="form-control" id="add" placeholder="Enter The full Address" name="add" required>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary" id="submit" name="submit">ADD</button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+    </center>
+    </form>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
--- Attendance/attendence.php
+++ Attendance/attendence.php
@@ -13,7 +13,7 @@ if (isset($_POST['submit'])) {
         $sub = $_POST["sub"];
         $sem = $_POST["sem"];
         mysqli_query($connect, "insert into attendence (usn,attend,subject,sem) values ('$usn','$attend','$sub','$sem')");
-        header("location:admin_dash.php?attend=success");
+        header("location:admin_dash.php");
     }
     // echo $sub;
 
@@ -32,18 +32,18 @@ mysqli_close($connect);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attandance</title>
     <link rel="stylesheet" href="css/style.css" />
-        <script>
-            function done(){
-                alert("Attendaance Successfully Taked..");
-            }
-        </script>
+    <script>
+        function done() {
+            alert("Attendaance Successfully Taked..");
+        }
+    </script>
 </head>
 
 <body style="background: #FC466B;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #3F5EFB, #FC466B);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #3F5EFB, #FC466B); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 ">
-<script>
+    <script>
         window.addEventListener("load", () => {
             const loader = document.querySelector(".loader");
 
@@ -56,7 +56,7 @@ background: linear-gradient(to right, #3F5EFB, #FC466B); /* W3C, IE 10+/ Edge, F
     </script>
     <link rel="stylesheet" href="css/loader.css">
     <div class="loader"></div>
-    <div style="border:0px;" class="header_fixed">  
+    <div style="border:0px;" class="header_fixed">
         <center>
             <!-- form method -->
             <form method="POST">
@@ -64,7 +64,6 @@ background: linear-gradient(to right, #3F5EFB, #FC466B); /* W3C, IE 10+/ Edge, F
                 <!-- select Starts from here -->
                 <h2> <label for="dub" style="line-height:normal;">Choose Sub</label></h2><br>
                 <select name="sub" style="border-radius: 15px;height: 35px;width: 193px; background:transparent;font-weight:900;border: 2px solid blue;text-align:center">
-                    <option value=""><b>---select Subject---</b></option>
                     <?php
                     while ($subdata = mysqli_fetch_array($subresult)) {
                         $subj = $subdata['sub_name'];
@@ -140,20 +139,20 @@ background: linear-gradient(to right, #3F5EFB, #FC466B); /* W3C, IE 10+/ Edge, F
                     <h3 id="abse"></h3>
                     <h3>Total present: </h3>
                 </div> -->
-                <!-- Submit Button Ends Here -->
-    </div>
+                    <!-- Submit Button Ends Here -->
+                </div>
 
-    </form>
-
-
+            </form>
 
 
-    </center>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+        </center>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
 </body>
 
