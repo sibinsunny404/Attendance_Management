16355070a306e27eb115c5652afc62bb56baf8fe
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-24 22:31:05 +0530

    Update login_process.php

--- Attendance/login_process.php
+++ Attendance/login_process.php
@@ -1,26 +1,23 @@
 <?php
 session_start();
 require_once 'database/connection.php';
-if(isset($_POST['login'])){
-     if(empty($_POST['uname']) || empty($_POST['pass'])){
+if (isset($_POST['login'])) {
+    if (empty($_POST['uname']) || empty($_POST['pass'])) {
         header("location:index.php?Empty=PLEASE FILL THE FEILDS");
-     }
-     else{
-        $query="select * from admin where admin_uname='".$_POST['uname']."' and passwd='".$_POST['pass']."'";
-        $result=mysqli_query($connect,$query);
-        if(mysqli_fetch_assoc($result)){
-            $_SESSION['user']=$_POST['uname'];
+    } else {
+        $query = "select * from admin where admin_uname='" . $_POST['uname'] . "' and passwd='" . $_POST['pass'] . "'";
+        $result = mysqli_query($connect, $query);
+        if (mysqli_fetch_assoc($result)) {
+            $_SESSION['user'] = $_POST['uname'];
             // sessin start time
-            $_SESSION['start']=time();
+            $_SESSION['start'] = time();
             // tacking session start time
-            $_SESSION['expire']=$_SESSION['start'] + (30*60);
+            $_SESSION['expire'] = $_SESSION['start'] + (30 * 60);
             header("location:admin_dash.php");
-        }
-        else{
+        } else {
             header("location:index.php?invalid=Enter The Valid Credentials");
         }
     }
-}
-else{
+} else {
     header("location:index.php");
 }
