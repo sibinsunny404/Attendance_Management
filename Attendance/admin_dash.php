57db24d86f6145f096d0fad213dacf5e4dbc89f6
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-25 22:48:06 +0530

    Update admin_dash.php

--- Attendance/admin_dash.php
+++ Attendance/admin_dash.php
@@ -275,7 +275,7 @@ require_once 'sessions.php';
 
 				<div class="app-card alert alert-dismissible shadow-sm mb-4 border-left-decoration" role="alert">
 					<div class="inner">
-						<div class="app-card-body p-3 p-lg-4">
+						<div class="app-card-body p-3 p-lg-6">
 							<h3 class="mb-3">Welcome <?php echo $_SESSION['user']; ?></h3>
 							<div class="row gx-5 gy-3">
 								<div class="col-12 col-lg-12">
