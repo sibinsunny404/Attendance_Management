0b4bfabb87e4d4ced062815f6f335a77c1344c6c
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-25 22:47:36 +0530

    updated

--- Attendance/admin_dash.php
+++ Attendance/admin_dash.php
@@ -269,17 +269,16 @@ require_once 'sessions.php';
 	<div class="app-wrapper">
 
 		<div class="app-content pt-3 p-md-3 p-lg-4">
-			<div class="container-xl">
+			<div class="container-xl" style="border:1px;">
 
-				<h1 class="app-page-title">Student Attendace System</h1>
+				<h1 class="app-page-title" style="color:white;">Student Attendace System</h1>
 
 				<div class="app-card alert alert-dismissible shadow-sm mb-4 border-left-decoration" role="alert">
 					<div class="inner">
 						<div class="app-card-body p-3 p-lg-4">
 							<h3 class="mb-3">Welcome <?php echo $_SESSION['user']; ?></h3>
 							<div class="row gx-5 gy-3">
-								<div class="col-12 col-lg-9">
-
+								<div class="col-12 col-lg-12">
 									<div>Welcome to The is the student attendace management system,</div>
 								</div>
 								<!--//col-->
