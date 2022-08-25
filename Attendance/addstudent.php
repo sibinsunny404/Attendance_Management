795228be5be05a85132fef6a06597205e5a8c6b8
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-25 17:23:20 +0530

    25/08//22

--- Attendance/addstudent.php
+++ Attendance/addstudent.php
@@ -26,8 +26,8 @@ if (isset($_POST['add'])) {
   <title>Add students</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+  <!-- /<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
+  <meta name="viewport" content="width=1024">
   <!-- Bootstrap CSS v5.2.0-beta1 -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <style>
