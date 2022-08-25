65c177faf47f0123469a0573780020e758bcdc2a
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-25 22:20:04 +0530

    update

--- Attendance/sample.php
+++ Attendance/sample.php
@@ -12,15 +12,11 @@
 
 </head>
 
-<body>
+<body style="background-color: black;">
   <?php
-
-  echo '<script type ="text/JavaScript">';
-  echo 'alert("JavaScript Alert Box by PHP")';
-  echo '</script>';
-
+ require_once 'loader.html'
   ?>
-  <!-- <a class="dropdown-item" href="logout.php?logout">Logout</a> --> -->
+  <!-- <a class="dropdown-item" href="logout.php?logout">Logout</a> --> 
 
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
