1aa5bf00b737bc24acbbb5985469febe31f10b70
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-28 13:08:29 +0530

    updated

--- Attendance/attendence.php
+++ Attendance/attendence.php
@@ -40,10 +40,7 @@ mysqli_close($connect);
     </script>
 </head>
 
-<body style="background: #FC466B;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #3F5EFB, #FC466B);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #3F5EFB, #FC466B); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-">
+<body style="background-image: linear-gradient(to top, #fff1eb 0%, #ace0f9 100%);">
     <script>
         window.addEventListener("load", () => {
             const loader = document.querySelector(".loader");
