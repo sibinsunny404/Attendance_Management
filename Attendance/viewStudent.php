4e21441af3e61c748217da8159666046717607aa
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-28 12:53:02 +0530

    Update viewStudent.php

--- Attendance/viewStudent.php
+++ Attendance/viewStudent.php
@@ -20,11 +20,12 @@ require_once 'database/data_retrive.php';
       background-color: #8BC6EC;
       background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
     }
-    h1{
+
+    h1 {
       text-align: center;
-     color:white;
-     background:grey;
-     display:block;
+      color: white;
+      background: grey;
+      display: block;
     }
   </style>
 </head>
