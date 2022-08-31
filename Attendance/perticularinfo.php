da56238846ecde36d6efc2748eecaa7f170146fc
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-31 22:59:17 +0530

    Update perticularinfo.php

--- Attendance/perticularinfo.php
+++ Attendance/perticularinfo.php
@@ -52,6 +52,17 @@ $sviewres = mysqli_query($connect, $sview);
   class:=<?php echo $cls ?>
   sem:=<?php echo $sem ?>
 
+<div class="container">
+  <div class="card mb-3">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+</div>
+
 
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
