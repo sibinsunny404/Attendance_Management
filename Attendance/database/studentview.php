ba8cb20b274d56bab0a6ba58f8545c8de2931778
Sibin Sunny <64060233+sibinsunny404@users.noreply.github.com>
2022-08-31 21:14:14 +0530

    Update studentview.php

--- Attendance/database/studentview.php
+++ Attendance/database/studentview.php
@@ -1,5 +1,5 @@
 <?php
     require_once 'connection.php';
-    $query="select count(*) from students";
-    $subresult = mysqli_query($connect, $query);
+    $sview="select * from students";
+    $sviewres = mysqli_query($connect, $sview);
 ?>
